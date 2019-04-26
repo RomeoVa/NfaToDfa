44662e4f4a1a568deefe274e99910f36dc93aed9
romeova <romeova.7243@gmail.com>
2019-04-25 19:19:04 -0500

    Initial Commit

---
+++ NfaToDfa/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NfaToDfa
+//
+//  Created by Romeo Varela on 4/25/19.
+//  Copyright © 2019 Romeo Varela. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
